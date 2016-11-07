abaf1c56ce76f62ec5fb55f18d9a3bb9ca751f60
ShiftUnion <nick.cahill@gmail.com>
2016-11-07 13:42:34 +1300

    Added some tests

--- sagas.spec.js
+++ sagas.spec.js
@@ -1,29 +1,28 @@
-import test from 'tape';
-
-import { put, call } from 'redux-saga/effects'
-import { delay } from 'redux-saga'
-import { incrementAsync } from './sagas'
+import test from "tape";
+import {put, call} from "redux-saga/effects";
+import {delay} from "redux-saga";
+import {incrementAsync} from "./sagas";
 
 test('incrementAsync Saga test', (assert) => {
-    const gen = incrementAsync()
+    const gen = incrementAsync();
 
     assert.deepEqual(
         gen.next().value,
         call(delay, 2000),
         'incrementAsync Saga must call delay(1000)'
-    )
+    );
 
     assert.deepEqual(
         gen.next().value,
         put({type: 'INCREMENT'}),
         'incrementAsync Saga must dispatch an INCREMENT action'
-    )
+    );
 
     assert.deepEqual(
         gen.next(),
-        { done: true, value: undefined },
+        {done: true, value: undefined},
         'incrementAsync Saga must be done'
-    )
+    );
 
     assert.end()
 });
